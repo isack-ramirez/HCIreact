6c4101b7dc5edfe5a105d19d42cf87579602ceb3
Isack Ramirez <isack.ramirez1@gmail.com>
2022-04-14 15:26:34 -0500

    happy with the landing banner, outside of the tweak to worry about for new flip phones its ready to go

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,32 +1,13 @@
 import { getAllByPlaceholderText } from "@testing-library/react";
 import React from "react";
-import { Container } from "react-bootstrap";
-import image from '../Assets/TEST.jpg';
+import Landingpicture from '../Components/Landingpicture'
 
 
 const Landing = () => {
   return (
-    <>
-        <div
-        style={{
-        minHeight:"100vh",
-        minWidth:'100vw',
-        
-      
-          backgroundImage:
-          
-            `url(${image})`,
-            backgroundSize: 'cover'
-           , 
-        }}
-        className="page-header"
-        data-parallax={true}
-      >
-      <Container>
-       
-      </Container>
-      </div>
-    </>
+    
+      <Landingpicture/>
+  
   );
 };
 
