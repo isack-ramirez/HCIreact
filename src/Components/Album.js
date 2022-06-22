d6dcb55cbb602a1724f5ec3a0287d23de917e596
Isack Ramirez <isack.ramirez1@gmail.com>
2022-06-22 18:02:38 -0500

    tweaked carousel of images to appropriate style, might require some more minor tweaking to look more natural

--- src/Components/Album.js
+++ src/Components/Album.js
@@ -1,32 +1,39 @@
-import React from 'react';
-import Album_styles from "../Components/styles/Services_styles.css";
-import { Carousel, Row } from 'react-bootstrap';
-import img_01 from '../Assets/img_01.jpg';
-import img_02 from '../Assets/img_02.jpg';
-import img_03 from '../Assets/img_03.jpg';
-import img_04 from '../Assets/img_04.jpg';
-import img_05 from '../Assets/img_05.jpg';
+import React from "react";
+import Album_styles from "../Components/styles/Album_styles.css";
+import { Carousel, Row } from "react-bootstrap";
+import img_01 from "../Assets/img_01.jpg";
+import img_02 from "../Assets/img_02.jpg";
+import img_03 from "../Assets/img_03.jpg";
+import img_04 from "../Assets/img_04.jpg";
+import img_05 from "../Assets/img_05.jpg";
 const Album = () => {
-    return (
-        <>
-  
-        <Carousel>
-           
+  return (
+    <>
+    <Row>
+      <Carousel id='album'>
         <Carousel.Item>
-          <img
-        
-            className="d-block w-100 h-50"
-            src={img_01}
-           alt='first slide'
-          />
+          <img className="d-block w-100" src={img_01} alt="first slide" />
         </Carousel.Item>
-        
-  
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={img_02} alt="first slide" />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={img_03} alt="first slide" />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={img_04} alt="first slide" />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={img_05} alt="first slide" />
+        </Carousel.Item>
       </Carousel>
-   
-    
-        </>
-    );
+      </Row>
+    </>
+  );
 };
 
 export default Album;
