71dc9b1af51741859394e1c9505c34abc8da21c1
Isack Ramirez <isack.ramirez1@gmail.com>
2022-07-06 17:38:49 -0500

    fixed style bug on album and started on contact page

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Services_styles from "../Components/styles/Services_styles.css";
+import Card from 'react-bootstrap/Card'
+const Services = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <h1 className="heading">Have a question? Contact us here :</h1>
+      </div>
+
+
+    </>
+  );
+};
+
+export default Services;
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -2,12 +2,14 @@ import React from "react";
 import Landingpicture from "../Components/Landingpicture";
 import Services from "../Components/Services";
 import Album from '../Components/Album';
+import Contact from '../Components/Contact';
 const Landing = () => {
   return(
     <>
    <Landingpicture />
    <Services/>
    <Album/>
+   <Contact/>
    </>
   )
 };
